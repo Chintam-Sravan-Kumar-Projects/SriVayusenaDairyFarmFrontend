bd305253788c237630e0410256bd9f93675e3c20
Chintam-Sravan-Kumar-Projects <chintamsravan78@gmail.com>
2025-02-18 12:29:31 +0530

    Update api.js

--- src/Redux/Api/api.js
+++ src/Redux/Api/api.js
@@ -1,8 +1,8 @@
 
 // export  const  url = process.env.REACT_APP_API_URL_cyclic;
 // export  const localhost =process.env.REACT_APP_API_URL_localhost ;
-export  const url2 =import.meta.env.VITE_URL/api;
-export const url = import.meta.env.VITE_URL/api;
-export const localhost = import.meta.env.VITE_URL/api;
+export  const url2 =import.meta.env.VITE_URL;
+export const url = import.meta.env.VITE_URL;
+export const localhost = import.meta.env.VITE_URL;
 
 console.log(url);
